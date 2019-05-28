042fd79f80b93289b662654fa87f41ad4c104018
Hong Ooi <hongooi@microsoft.com>
2019-05-28 21:36:13 +1000

    fix args

--- R/managed_token.R
+++ R/managed_token.R
@@ -2,6 +2,5 @@ get_managed_token <- function(resource)
 {
     auth_type <- "managed"
     aad_host <- "http://169.254.169.254/metadata/identity/oauth2"
-    AzureTokenV1$new(resource, tenant="common", app, password, username, certificate, auth_type, aad_host,
-                     authorize_args, token_args, on_behalf_of)
+    AzureTokenV1$new(resource, tenant="common", app=NULL, auth_type=auth_type, aad_host=aad_host)
 }
