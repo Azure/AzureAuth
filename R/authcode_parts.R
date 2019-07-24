88f46e032130fec299824b42cdce3cc93df78c01
Hong Ooi <hongooi@microsoft.com>
2019-07-24 10:02:18 +1000

    jwt methods

--- R/authcode_parts.R
+++ R/authcode_parts.R
@@ -1,9 +1,22 @@
+#' Generate URI for the OAuth authorization code flow
+#'
+#' @param endpoint The base URL for the Azure Active Directory login endpoint, or an `aad_endpoint` object.
+#' @param tenant Your tenant. This can be a name ("myaadtenant"), a fully qualified domain name ("myaadtenant.onmicrosoft.com" or "mycompanyname.com"), or a GUID.
+#' @param app The client/app ID to use to authenticate with.
+#' @param version The AAD version, either 1 or 2.
+#' @param ... Named arguments that will be added to the authorization URI as query parameters.
+#' @param .params Alternatively, a named list of query parameters. If supplied, this overrides arguments supplied in `...`.
+#'
+#' @return
+#' An object of class `httr::url`, representing the _parsed_ authorization URI. You can call `httr::build_url` on this object to obtain the URI as a text string.
+#' @export
 aad_authorize_uri <- function(endpoint, ...)
 {
     UseMethod("aad_authorize_uri")
 }
 
 
+#' @export
 aad_authorize_uri.default <- function(endpoint="https://login.microsoftonline.com/", tenant=NULL, app=NULL, version=1,
                                       ..., .params=list(...))
 {
@@ -12,6 +25,7 @@ aad_authorize_uri.default <- function(endpoint="https://login.microsoftonline.co
 }
 
 
+#' @export
 aad_authorize_uri.aad_endpoint <- function(endpoint, app=NULL, ..., .params=list(...))
 {
     uri <- httr::parse_url(endpoint)
--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,14 @@
-#' Decode info in a token (which is a JWT object)
+#' Get raw access token (which is a JWT object)
 #'
-#' @param token A string representing the encoded token.
+#' @param token A token object. This can be an object of class `AzureToken`, of class `httr::Token`, or a character string containing the encoded token.
 #'
 #' @details
-#' An OAuth token is a _JSON Web Token_, which is a set of base64URL-encoded JSON objects containing the token credentials along with an optional (opaque) verification signature. `decode_jwt` decodes the credentials into an R object so they can be viewed.
+#' An OAuth token is a _JSON Web Token_, which is a set of base64URL-encoded JSON objects containing the token credentials along with an optional (opaque) verification signature. `decode_jwt` decodes the credentials into an R object so they can be viewed. `extract_jwt` extracts the credentials from an R object of class `AzureToken` or `httr::Token`.
 #'
 #' Note that `decode_jwt` does not touch the token signature or attempt to verify the credentials. You should not rely on the decoded information without verifying it independently. Passing the token itself to Azure is safe, as Azure will carry out its own verification procedure.
 #'
 #' @return
-#' A list containing up to 3 components: `header`, `payload` and `signature`.
+#' For `extract_jwt`, the character string containing the encoded token, suitable for including in a HTTP query. For `decode_jwt`, a list containing up to 3 components: `header`, `payload` and `signature`.
 #'
 #' @seealso
 #' [jwt.io](https://jwt.io), the main JWT informational site
@@ -16,8 +16,33 @@
 #' [jwt.ms](https://jwt.ms), Microsoft site to decode and explain JWTs
 #'
 #' [JWT Wikipedia entry](https://en.wikipedia.org/wiki/JSON_Web_Token)
+#' @rdname jwt
 #' @export
 decode_jwt <- function(token)
+{
+    UseMethod("decode_jwt")
+}
+
+
+#' @rdname jwt
+#' @export
+decode_jwt.AzureToken <- function(token)
+{
+    decode_jwt(token$credentials$access_token)
+}
+
+
+#' @rdname jwt
+#' @export
+decode_jwt.Token <- function(token)
+{
+    decode_jwt(token$credentials$access_token)
+}
+
+
+#' @rdname jwt
+#' @export
+decode_jwt.character <- function(token)
 {
     token <- as.list(strsplit(token, "\\.")[[1]])
     token[1:2] <- lapply(token[1:2], function(x)
@@ -27,6 +52,38 @@ decode_jwt <- function(token)
     if(length(token) > 2)
         names(token)[3] <- "signature"
 
+    token
+}
+
+
+#' @rdname jwt
+#' @export
+extract_jwt <- function(token)
+{
+    UseMethod("extract_jwt")
+}
+
+
+#' @rdname jwt
+#' @export
+extract_jwt.AzureToken <- function(token)
+{
+    token$credentials$access_token
+}
+
+
+#' @rdname jwt
+#' @export
+extract_jwt.Token <- function(token)
+{
+    token$credentials$access_token
+}
+
+
+#' @rdname jwt
+#' @export
+extract_jwt.character <- function(token)
+{
     token
 }
 
